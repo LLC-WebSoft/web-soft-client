c9797a1f5cf665a37269092e10c56865a23f0e5e
DanilSord <kincharov99@gmail.com>
2022-03-08 13:08:49 +0700

    add: validation with ajv

--- index.js
+++ index.js
@@ -1,59 +1,8 @@
-'use strict'
+'use strict';
+import { HTTPConnection } from './lib/http-connection';
+import { WSConnection } from './lib/ws-connection';
 
-class Connection {
-  constructor() {
-    this.callId = 0;
-  }
-
-  async call(method = '', params = {}) {
-    const data = {jsonrpc: '2.0', method, params, id: this.callId};
-    this.callId++;
-    return await this.request(data);
-  }
-}
-
-const FETCH_OPRIONS = {
-  method: 'POST',
-  mode: 'cors',
-  credentials: 'include',
-  cache: 'no-cache',
-  redirect: 'follow',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-};
-
-class HTTPConnection extends Connection{
-  constructor(host, secure = false) {
-    super();
-    this.host = host;
-    this.secure = secure;
-    this.url = `http${ this.secure ? 's' : '' }://${this.host}`;
-  }
-
-  async request(data) {
-    const result = await fetch(this.url, { ...FETCH_OPRIONS, body: JSON.stringify(data)});
-    if (result.status !== 200 || result.headers.get('Content-Type') !== 'application/json') {
-      throw new Error('Invalid server response.');
-    }
-    return await result.json();
-  }
-}
-
-class WSConnection extends Connection{
-  constructor(host, secure = false) {
-    super();
-    this.host = host;
-    this.secure = secure;
-    this.url = `ws${ this.secure ? 's' : '' }://${this.host}`;
-    this.client = new WebSocket(this.url);
-  }
-
-  async request(data) {
-    this.client.send(JSON.stringify(data));
-    return await result.json();
-  }
-}
-
-const connection = new HTTPConnection('http://localhost');
-connection.call('auth/me').then((result) => console.log(result));
+const httpConnection = new HTTPConnection('localhost');
+const wsConnection = new WSConnection('localhost');
+httpConnection.call('auth/me');
+wsConnection.call('auth/me');
