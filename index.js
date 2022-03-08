b2d7713f454dff81574f4d6ac76f7b13bd3c4a27
DanilSord <kincharov99@gmail.com>
2022-03-08 17:31:06 +0700

    fix: resolveOne before request was queued

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ import { WSConnection } from './lib/ws-connection';
 
 const httpConnection = new HTTPConnection('localhost');
 const wsConnection = new WSConnection('localhost');
-httpConnection.call('auth/me');
-wsConnection.call('auth/me');
+
+(async () => {
+  const res1 = await httpConnection.call('auth/me');
+  const res2 = await wsConnection.call('auth/me');
+  console.log(res1, res2);
+})();
